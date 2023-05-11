8f947f58b8512dad47a06c5ec1ad8d9bcc9550a5
higor-xavier <higorsxavier@gmail.com>
2023-05-11 15:34:34 -0300

    Following and unfollowing users

--- App/Route.php
+++ App/Route.php
@@ -56,6 +56,12 @@ class Route extends Bootstrap {
 			'action' => 'quem_seguir'
 		);
 
+		$routes['acao'] = array(
+			'route' => '/acao',
+			'controller' => 'AppController',
+			'action' => 'acao'
+		);
+
 		$this->setRoutes($routes);
 	}
 
